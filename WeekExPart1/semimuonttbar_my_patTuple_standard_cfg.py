3fbbda19d5f78cce29dfb0032f40f7c6a1f15c16
fhohle <>
2011-12-04 13:54:52 +0000

    genmatching switched off

--- WeekExPart1/semimuonttbar_my_patTuple_standard_cfg.py
+++ WeekExPart1/semimuonttbar_my_patTuple_standard_cfg.py
@@ -8,7 +8,7 @@ from PhysicsTools.PatAlgos.patTemplate_cfg import *
 from PhysicsTools.PatAlgos.tools.coreTools import *
 
 ## remove MC matching from the default sequence
-# removeMCMatching(process, ['Muons'])
+RemoveMCMatching(process, ['All'])
 # runOnData(process)
 
 ## remove certain objects from the default sequence
